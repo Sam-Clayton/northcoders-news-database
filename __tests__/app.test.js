ec1ad74cef95405d7a85f5acd464c3f56227cc7f
Sam <sam-clayton@hotmail.co.uk>
2025-07-23 16:41:55 +0100

    set up model-view-controller and began testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,44 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+const app = require("../app.js");
+const request = require("supertest");
 
-/* Set up your beforeEach & afterAll functions here */
+beforeEach(()=>{
+    return seed(data)
+});
 
-describe("GET /api", () => {
-  test.skip("200: Responds with an object detailing the documentation for each endpoint", () => {
+afterAll(()=>{
+    return db.end()
+});
+
+describe.skip("GET /api", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
+      });
+  });
+});
+
+describe("GET /api/topics", () => {
+  test("200: Responds with topic objects", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        console.log({topics}, '<<<<<')
+        expect(topics).toHaveLength(3);
+        topics.forEach((topic) => {
+          expect(topic).toMatchObject({
+            slug: expect.any(String),
+            description: expect.any(String),
+            img_url: expect.any(String)
+          })
+        })
       });
   });
 });
---
+++ controllers/topics.controllers.js
@@ -0,0 +1,10 @@
+const fetchTopics = require('../models/topics.models');
+
+const getTopics = (req, res) => {
+    fetchTopics().then((topics) => {
+        res.status(200)
+        .send({topics})
+    })
+};
+
+module.exports = getTopics;
